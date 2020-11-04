f32cd0dc596f7e51b5196eb8fdf4feeb1d5744e5
Maximilian Gruber <maximilian.gruber@ptb.de>
2020-11-04 21:14:50 +0100

    wip

--- base.py
+++ base.py
@@ -12,10 +12,10 @@ class PhysicalPhenomenon:
         times = np.linspace(0, 5, 200)
         chirp = scs.chirp(times, f0=1, t1=5, f1=3)
 
-        self.base_signal = 0.1 * np.concatenate((chirp, chirp[::-1]))
+        self.base_signal = 1 * np.concatenate((chirp, chirp[::-1]))
         self.counter = 0
         self.p_new_offset = 0.02
-        self.offset = 0
+        self.offset = 10
 
     def value(self, time):
         value = self.base_signal[self.counter] + self.offset
@@ -30,13 +30,21 @@ class PhysicalPhenomenon:
             self.counter = 0
 
         return value
+        #return 10 + 0.01 * np.random.randn()
 
 
 class Sensor:
-    def __init__(self, transfer_model, estimated_transfer_model=None, estimated_compensation_model=None):
+    def __init__(
+        self,
+        transfer_model,
+        estimated_transfer_model=None,
+        estimated_compensation_model=None,
+    ):
         self.transfer_model = transfer_model  # simulation model
         self.estimated_transfer_model = estimated_transfer_model  # calibration model
-        self.estimated_compensation_model = estimated_compensation_model  # compensation model
+        self.estimated_compensation_model = (
+            estimated_compensation_model  # compensation model
+        )
 
     def indicated_value(self, physical_phenomenon_value):
         value, value_unc = self.transfer_model.apply(physical_phenomenon_value, 0)
@@ -55,11 +63,11 @@ class ParametricModel:
     Allows to define a structure with parametrization, that can then be applied to some input(-signal).
     """
 
-    def __init__(self, evaluation_function, parameters, parameters_uncertainty):
+    def __init__(self, evaluation_function, parameters, parameters_uncertainty, state=None):
         self.evaluation_function = evaluation_function
         self.parameters = parameters
         self.parameters_uncertainty = parameters_uncertainty
-        self.state = None
+        self.state = state
 
     def apply(self, input, input_unc):
         output, output_uncertainty, self.state = self.evaluation_function(
@@ -69,19 +77,26 @@ class ParametricModel:
 
     def inverse_model_parameters(self):
         return None
-    
+
     def set_parameters(self, parameters=None, parameters_uncertainty=None):
         if parameters is not None:
             self.parameters = parameters
-        
+
         if parameters_uncertainty is not None:
             self.parameters_uncertainty = parameters_uncertainty
 
+
 class LinearAffineModel(ParametricModel):
-    def __init__(self, a=1.0, b=0.0, ua=0.0, ub=0.0):
+    def __init__(self, a=1.0, b=0.0, ua=0.0, ub=0.0, uab=0.0):
         """Initialize linear affine model with uncorrelated uncertainty evaluation
         y = a * x + b
-        uy = sqrt(x^2 * ua^2 + a^2 * ux^2 + ub^2)
+        uy = sqrt(C * U * C^T)
+
+        with
+        C = [dy/da, dy/db, dy/dx]^T = [x, 1, a]^T
+        U = [[ua^2, uab,  0   ]
+             [uab,  ub^2, 0   ]
+             [0,    0,    ux^2]]
 
         :param a: gain
         :type a: float
@@ -91,14 +106,18 @@ class LinearAffineModel(ParametricModel):
         :type ua: float, optional
         :param ub: offset uncertainty, defaults to 0.0
         :type ub: float, optional
+        :param uab: correlated uncertainty between a and b, defaults to 0.0
+        :type ub: float, optional
         """
 
         evaluation_function = self.equation
         parameter = [a, b]
-        parameters_uncertainty = [ua, ub]
+        parameters_uncertainty = np.array(
+            [[np.square(ua), uab], [uab, np.square(ub)]]
+        )  # covariance matrix
 
         super().__init__(evaluation_function, parameter, parameters_uncertainty)
-    
+
     def __repr__(self):
         # shortcuts
         a = self.parameters[0]
@@ -111,12 +130,19 @@ class LinearAffineModel(ParametricModel):
         # shortcuts
         a = p[0]
         b = p[1]
-        ua = up[0]
-        ub = up[1]
+        ua2 = up[0, 0]
+        ub2 = up[1, 1]
+        uab = up[0, 1]
+
+        # sensitivities
+        C = np.array([x, 1, a])
+
+        # uncertainty matrix
+        U = np.array([[ua2, uab, 0], [uab, ub2, 0], [0, 0, np.square(ux)]])
 
         # output equation
         val = a * x + b
-        unc = np.sqrt(np.square(x * ua) + np.square(a * ux) + np.square(ub))
+        unc = np.sqrt(C @ U @ C.T)
         state = None
 
         return val, unc, state
@@ -125,25 +151,27 @@ class LinearAffineModel(ParametricModel):
         # shortcuts
         a = self.parameters[0]
         b = self.parameters[1]
-        ua = self.parameters_uncertainty[0]
-        ub = self.parameters_uncertainty[1]
+        ua2 = self.parameters_uncertainty[0, 0]
+        ub2 = self.parameters_uncertainty[1, 1]
+        uab = self.parameters_uncertainty[0, 1]
 
         # inverse (unc according GUM)
         a_inv = 1 / a
         b_inv = -b / a
-        ua_inv = ua / np.square(a)
-        ub_inv = np.sqrt(
-            1 / np.square(a) * np.square(ub)
-            + np.square(b / np.square(a)) * np.square(ua)
-        )
+
+        C = np.array([[-1 / np.square(a), 0], [b / np.square(a), -1 / a]])
+        U = np.array([[ua2, uab], [uab, ub2]])
+        U_inv = C @ U @ C.T
 
         # pack result
+        ua_inv = np.sqrt(U_inv[0, 0])
+        ub_inv = np.sqrt(U_inv[1, 1])
+        uab_inv = U_inv[0, 1]
         parameters_inv = {"a": a_inv, "b": b_inv}
-        parameters_uncertainty_inv = {"ua": ua_inv, "ub": ub_inv}
+        parameters_uncertainty_inv = {"ua": ua_inv, "ub": ub_inv, "uab": uab_inv}
 
         return parameters_inv, parameters_uncertainty_inv
 
 
 class LinearTimeInvariantModel(ParametricModel):
     pass
-
--- recreate_stankovic.py
+++ recreate_stankovic.py
@@ -12,6 +12,38 @@ from base import LinearAffineModel, PhysicalPhenomenon, Sensor
 n_neighbors = 5
 maxlen = 1000
 neighbors = []
+delta = 0.0001
+
+
+# WIP: these functions should probably be defined somewhere else inside base?
+def build_derivative(param, neighbor_value_estimates, weights, y, delta):
+    nn = len(neighbor_value_estimates)
+    shape = (2, 2 + nn + 1)
+    C = np.zeros(shape)
+    a = param[0]
+    b = param[1]
+
+    # derivatives
+    C[0, 0] = 1 + delta * np.sum(weights * (-np.square(y)))
+    C[0, 1] = 0 + delta * np.sum(weights * (-y))
+    C[0, 2 : 2 + nn] = 0 + delta * weights * y
+    C[0, -1] = 0 + delta * np.sum(weights * (-2 * a * y + neighbor_value_estimates - b))
+
+    C[1, 0] = 0 + delta * np.sum(weights * (-y))
+    C[1, 1] = 1 + delta * np.sum(weights * (-1))
+    C[1, 2 : 2 + nn] = 0 + delta * weights
+    C[1, -1] = 0 + delta * np.sum(weights * (-a))
+
+    return C
+
+
+def build_full_input_uncertainty_matrix(param_unc, neighbor_uncertainty_estimates, uy):
+    main_diag = np.hstack((np.diag(param_unc), neighbor_uncertainty_estimates, uy))
+    U = np.diag(main_diag)
+    U[:2, :2] = param_unc
+
+    return U
+
 
 for i in range(n_neighbors):
 
@@ -40,7 +72,7 @@ for i in range(n_neighbors):
 
 # introduce new sensor
 transfer = LinearAffineModel(a=1.2, b=7.7)
-inverse = LinearAffineModel()
+inverse = LinearAffineModel(a=1, b=-5, ua=1.0, ub=1.0, uab=0)
 sensor = Sensor(transfer_model=transfer, estimated_compensation_model=inverse)
 buffer_indication = TimeSeriesBuffer(maxlen=maxlen, return_type="arrays")
 buffer_estimation = TimeSeriesBuffer(maxlen=maxlen, return_type="arrays")
@@ -49,13 +81,14 @@ new_neighbor = {
     "buffer_indication": buffer_indication,
     "buffer_estimation": buffer_estimation,
 }
+buffer_parameters = TimeSeriesBuffer(maxlen=maxlen, return_type="list")
 
 # init physical phenomenon that will be observed by sensors
 pp = PhysicalPhenomenon()
 pp_buffer = TimeSeriesBuffer(maxlen=maxlen, return_type="arrays")
 
 # observe the signal for some time
-time = np.linspace(0, 20, 1000)
+time = np.linspace(0, 20, 500)
 for t in time:
     # actual value of physical phenomenon
     real_value = pp.value(t)
@@ -64,46 +97,79 @@ for t in time:
     # simulate sensor readings
     for n in neighbors + [new_neighbor]:
         y, uy = n["sensor"].indicated_value(real_value)
-        y = y + uy * np.random.randn()
+        # y = y + uy * np.random.randn()
         n["buffer_indication"].add(data=[[t, y, uy]])
 
         x_hat, ux_hat = n["sensor"].estimated_value(y, uy)
         n["buffer_estimation"].add(data=[[t, x_hat, ux_hat]])
 
-    # adjust/compensate new sensor by adjusting its parameters based on gradient 
+    # adjust/compensate new sensor by adjusting its parameters based on gradient
     neighbor_value_estimates = np.squeeze(
         [n["buffer_estimation"].show(1)[2] for n in neighbors]
     )
     neighbor_uncertainty_estimates = np.squeeze(
         [n["buffer_estimation"].show(1)[3] for n in neighbors]
     )
-    J = np.sum(
-        np.square(neighbor_value_estimates - x_hat) / neighbor_uncertainty_estimates
-    )
-    grad_J = np.sum(
-        (neighbor_value_estimates - x_hat)
-        * np.array([[y, 1]]).T
-        / neighbor_uncertainty_estimates,
-        axis=1,
-    )
-    model = new_neighbor["sensor"].estimated_compensation_model
-    param = model.parameters
-    new_param = param + 0.001 * grad_J
-    print(param, J)
 
-    # todo: adjust parameter estimation uncertainties
+    delay = 5
+    if len(new_neighbor["buffer_indication"]) > delay:
+        y_delayed = new_neighbor["buffer_indication"].show(delay)[2][0]
+        # J = np.sum(np.square(neighbor_value_estimates - x_hat) / neighbor_uncertainty_estimates)
+        weights = neighbor_uncertainty_estimates / np.linalg.norm(
+            neighbor_uncertainty_estimates
+        )
+        grad_J = np.sum(
+            (neighbor_value_estimates - x_hat) * np.array([[y_delayed, 1]]).T / weights,
+            axis=1,
+        )
+        model = new_neighbor["sensor"].estimated_compensation_model
+        param = model.parameters
+        new_param = param + delta * grad_J
 
-    model.set_parameters(parameters = new_param)
+        # adjust parameter estimation uncertainties
+        C = build_derivative(
+            param, neighbor_value_estimates, weights, y, delta
+        )
+        U = build_full_input_uncertainty_matrix(
+            model.parameters_uncertainty, neighbor_uncertainty_estimates, uy
+        )
+        new_param_unc = C @ U @ C.T
+        # print(param, J)
+
+        model.set_parameters(parameters=new_param, parameters_uncertainty=new_param_unc)
+
+        # calculate estimated inverse model
+        p_inv, up_inv = model.inverse_model_parameters()
+        new_neighbor["sensor"].estimated_transfer_model = LinearAffineModel(
+            **p_inv, **up_inv
+        )
+        # print(new_neighbor["sensor"].estimated_transfer_model.parameters)
+        buffer_parameters.add(data=[[t, p_inv, up_inv]])
 
 
 # plotting
 tp, _, vp, _ = pp_buffer.show(-1)
-plt.plot(tp, vp, label="ground truth")
+
+fig, ax = plt.subplots(nrows=2, sharex=True)
+
+ax[0].plot(tp, vp, label="ground truth")
 for i, n in enumerate(neighbors + [new_neighbor]):
     tn, utn, vn, uvn = n["buffer_estimation"].show(-1)
-    plt.errorbar(
+    ax[0].errorbar(
         tn, vn, xerr=utn, yerr=uvn, errorevery=10, capsize=3, label=str(i)
     )  # , ecolor="grey")
+ax[0].legend()
 
-plt.legend()
+# show parameter development
+params_list = buffer_parameters.show(-1)
+t_par = [p[0] for p in params_list]
+par = np.array([(p[2]["a"], p[2]["b"]) for p in params_list])
+upar = np.array([(p[3]["ua"], p[3]["ub"]) for p in params_list])
+
+ax[1].plot(t_par, par, label="estimates")
+ax[1].errorbar(t_par, par[:,0], yerr=upar[:,0], errorevery=10, capsize=3, label="a")
+ax[1].errorbar(t_par, par[:,1], yerr=upar[:,1], errorevery=10, capsize=3, label="b")
+ax[1].hlines(new_neighbor["sensor"].transfer_model.parameters, tp.min(), tp.max())
+ax[1].legend()
+
 plt.show()
