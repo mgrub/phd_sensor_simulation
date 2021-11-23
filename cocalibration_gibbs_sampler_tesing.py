9eb590bfa3420e08019dde42582a1101c04ebf7d
Maximilian Gruber <maximilian.gruber@ptb.de>
2021-11-23 12:07:57 +0100

    adjust data generation model to formulas

--- cocalibration_gibbs_sampler_tesing.py
+++ cocalibration_gibbs_sampler_tesing.py
@@ -4,29 +4,17 @@ import scipy.optimize as sco
 
 from models import LinearAffineModel
 
-
+# actual ground truth
 t = np.linspace(0, 10, 101)
-x_true = np.sin(t) + 1
+x_actual = np.sin(t) + 1
 
+# reference values
+Ux = np.diag(np.full_like(x_actual, 0.01))
+x_observed = np.random.multivariate_normal(mean=x_actual, cov=Ux)
 
-# reference measurement simulation
-transfer_ref = LinearAffineModel(a=2.0, b=2.4, ua=0.1, ub=0.1, uab=0.005)
-transfer_ref_inverse = LinearAffineModel(**transfer_ref.inverse_model_parameters())
-
-## simulate data acquisition for reference
-y_ref, uy_ref = transfer_ref.apply(x_true, np.zeros_like(x_true))
-y_ref += uy_ref * np.random.randn(len(y_ref))  # add corresponding noise
-
-## simulate compensation of reference data
-x_ref, ux_ref = transfer_ref_inverse.apply(y_ref, uy_ref)
-
-
-# device-under-test (dut) measurement simulation
-transfer_dut = LinearAffineModel(a=1.7, b=0.7, ua=0.2, ub=0.2, uab=0.01)   # this is unknown later
-y_dut, uy_dut = transfer_dut.apply(x_true, np.zeros_like(x_true))
-y_dut += uy_dut * np.random.randn(len(y_dut))  # add corresponding noise
-transfer_dut_calib = LinearAffineModel(a=1, b=0, ua=1, ub=1, uab=0)
-
+# indication of DUT
+true_transfer_dut = LinearAffineModel(a=2.0, b=2.4)
+y = true_transfer_dut.apply(x_actual, np.zeros_like(x_actual))[0] + np.random.normal(scale=0.2, size=len(x_actual))
 
 
 # prepare some plotting
@@ -44,14 +32,13 @@ for current_indices in np.split(np.arange(len(t)), split_indices):
     # available measurement information
     tt = t[current_indices]
     print(tt[0])
-    xx_ref = x_ref[current_indices]
-    uxx_ref = ux_ref[current_indices]
-    #yy_ref = y_ref[current_indices]
-    #uyy_ref = uy_ref[current_indices]
-    yy_dut = y_dut[current_indices]
+    xx_actual = x_actual[current_indices]
+    xx_observed = x_observed[current_indices]
+    Uxx = Ux[current_indices][:,current_indices]
+    yy = y[current_indices]
 
     # plt
-    ax[0].scatter(tt, yy_dut)
+    ax[0].scatter(tt, yy)
 
     # update posteriors using (block-)Gibbs sampling
     # samples = []
@@ -71,13 +58,12 @@ for current_indices in np.split(np.arange(len(t)), split_indices):
     # transfer_dut_calib.parameters_uncertainty = S3
 
 
-ax[0].plot(t, x_true, label="quantity")
-#ax[0].errorbar(t, y_ref, uy_ref, label="ref measured")
-ax[0].errorbar(t, x_ref, ux_ref, label="ref compensated")
+ax[0].plot(t, x_actual, label="ground truth")
+ax[0].errorbar(t, x_observed, np.sqrt(np.diag(Ux)), label="reference signal")
+ax[0].plot(t, y, label="dut indication")
 
-ax[0].plot(t, y_dut, label="dut measured")
-transfer_dut_calib_inverse = LinearAffineModel(**transfer_dut_calib.inverse_model_parameters())
-ax[0].errorbar(t, *transfer_dut_calib_inverse.apply(y_dut, np.zeros_like(y_dut)), label="dut compensated")
+#transfer_dut_calib_inverse = LinearAffineModel(**transfer_dut_calib.inverse_model_parameters())
+#ax[0].errorbar(t, *transfer_dut_calib_inverse.apply(y_dut, np.zeros_like(y_dut)), label="dut compensated")
 
 ax[0].legend()
 
