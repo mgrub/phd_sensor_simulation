b50c6bc22c076b82c4e941bad27c93b4bb68042a
Maximilian Gruber <maximilian.gruber@ptb.de>
2022-01-02 17:28:32 +0100

    save

--- test_likelihood_approximation.py
+++ test_likelihood_approximation.py
@@ -8,21 +8,14 @@ mean_prior = np.array([1,2])
 cov_prior = np.array([[2,0.0],[0.0,1]])
 theta_dist = scs.multivariate_normal(mean = mean_prior, cov= cov_prior)
 
-
+# signals
 t = np.linspace(0, 10, 101)
 x_true = np.sin(t) + 1
 x_ref = x_true + 0.1 * np.random.randn(len(t))
 y_ind = 1.5 * x_true + 1.8  + 0.2 * np.random.randn(len(t))
 
-
-fig, ax = plt.subplots(1, 2)
-ax[0].plot(t, x_true, label="x true")
-ax[0].plot(t, x_ref, label="x ref")
-ax[0].plot(t, y_ind, label="y ind")
-ax[0].legend()
-
-
-samples = theta_dist.rvs(size=1000)
+# MC
+samples = theta_dist.rvs(size=10000)
 samples_likelihood = []
 for theta in samples:
     y_ind_model = theta[0] * x_ref + theta[1]
@@ -30,8 +23,8 @@ for theta in samples:
     p_X_theta = 1/np.linalg.norm(y_ind_model - y_ind)
     p_theta = theta_dist.pdf(theta)
     
-    samples_likelihood.append(p_X_theta * p_theta)
-    #samples_likelihood.append(p_X_theta)
+    #samples_likelihood.append(p_X_theta * p_theta)  # prop to posterior
+    samples_likelihood.append(p_X_theta)   # prop to likelihood
 
 # estimate p(X|theta) from MC
 samples_likelihood = np.array(samples_likelihood)
@@ -50,27 +43,44 @@ res = sco.minimize(evaluate, x0 = mean_prior, args=(x_ref, y_ind))
 mean_MLE = res.x
 cov_MLE = res.hess_inv
 
-# plotting
+
+# plot time series
+fig1, ax1 = plt.subplots(1, 1)
+ax1.plot(t, x_true, label="x true")
+ax1.plot(t, x_ref, label="x ref")
+ax1.plot(t, y_ind, label="y ind")
+ax1.set_title("input time series")
+ax1.legend()
+
+
+# plot prior and likelihood
+fig2, ax2 = plt.subplots(1, 1)
 # sample evaluation
-ax[1].scatter(samples[:,0], samples[:,1], c=samples_likelihood)
-fig.colorbar(cm.ScalarMappable(), ax=ax[1])
+ax2.scatter(samples[:,0], samples[:,1], c=samples_likelihood)
+fig2.colorbar(cm.ScalarMappable(), ax=ax2)
 
 # circle
 r = np.linspace(0,2*np.pi,100)
 circle = np.array([[np.cos(_r), np.sin(_r)] for _r in r]).T
 
-# estimated posterior distribution
-ell_likelihood =  (cov_likelihood@circle).T + mean_likelihood
-ax[1].plot(ell_likelihood[:,0], ell_likelihood[:,1], ":r")
-
 # estimated prior distribution
 ell_prior =  (cov_prior@circle).T + mean_prior
-ax[1].plot(ell_prior[:,0], ell_prior[:,1], ":b")
+ax2.plot(ell_prior[:,0], ell_prior[:,1], ":b", label="p(theta|alpha)")
+
+# estimated posterior distribution
+ell_likelihood =  (cov_likelihood@circle).T + mean_likelihood
+ax2.plot(ell_likelihood[:,0], ell_likelihood[:,1], ":r", label="p(X|theta) (MC + gaussian kde)")
 
 # maximum likelihood optimiziation distribution
 ell_MLE =  (cov_MLE@circle).T + mean_MLE
-ax[1].plot(ell_MLE[:,0], ell_MLE[:,1], ":g")
+ax2.plot(ell_MLE[:,0], ell_MLE[:,1], ":g", label="p(X|theta) (MLE via hess_inv of opt)")
 
 
-ax[1].set_aspect("equal")
+ax2.set_aspect("equal")
+ax2.legend()
+ax2.set_title("visualize prior and likelihood")
+ax2.set_xlabel("a = theta[0]")
+ax2.set_ylabel("b = theta[1]")
+
+
 plt.show()
